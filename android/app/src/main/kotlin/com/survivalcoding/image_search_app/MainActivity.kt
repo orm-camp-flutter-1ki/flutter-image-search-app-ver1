42572a77508339e82a775ed0564aae76438547c8
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-27 11:12:42 +0900

    feat: UI 뼈대 작성

---
+++ android/app/src/main/kotlin/com/survivalcoding/image_search_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.survivalcoding.image_search_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const TextField(),
+            Expanded(
+              child: GridView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Text('111');
+                },
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
