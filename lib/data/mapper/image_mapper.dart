523802158986e67823d6331e8910fcd923709d4e
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-27 12:21:02 +0900

    feat: Mapper 작성

---
+++ lib/data/mapper/image_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:image_search_app/data/dto/pixabay_dto.dart';
+
+import '../model/image_item.dart';
+
+extension DtoToModel on Hits {
+  ImageItem toImageItem() {
+    return ImageItem(
+      imageUrl: previewURL ??
+          'https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_org.jpg?61e575e8653e5920470a38d1482d7312/melon/optimize/90',
+      tags: tags ?? '',
+    );
+  }
+}
