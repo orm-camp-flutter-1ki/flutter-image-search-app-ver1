2d266f7499fb2e2a6286abc5c986869ffdd82b65
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-27 12:17:17 +0900

    feat: Data Source 작성

---
+++ lib/data/data_source/pixabay_api.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:image_search_app/data/dto/pixabay_dto.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayApi {
+  Future<PixabayDto> getImagesResult(String query) async {
+    final response = await http.get(Uri.parse(
+        'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo'));
+    return PixabayDto.fromJson(jsonDecode(response.body));
+  }
+}
