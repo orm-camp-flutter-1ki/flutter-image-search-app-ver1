6f652b0a4083f916af1dedaea2926d3cd95c90f5
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-12-29 16:36:21 +0900

    fix: 수정 안 되는 List

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -1,14 +1,19 @@
+import 'dart:collection';
+
 import '../data/model/image_item.dart';
 import '../data/repository/image_item_repository.dart';
 
 class MainViewModel {
   final repository = PixabayImageItemRepository();
 
-  List<ImageItem> imageItems = [];
+  List<ImageItem> _imageItems = [];
+
+  // UnmodifiableListView 수정 안 되는 리스트
+  List<ImageItem> get imageItems => List.unmodifiable(_imageItems);
 
   bool isLoading = false;
 
   Future<void> searchImage(String query) async {
-    imageItems = await repository.getImageItems(query);
+    _imageItems = await repository.getImageItems(query);
   }
 }
